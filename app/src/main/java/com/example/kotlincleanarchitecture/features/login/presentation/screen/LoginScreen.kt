120c551761e588c50a933e8d05169221833625a3
quangpham <phamminhquang.work@gmail.com>
2025-09-10 22:19:31 +0700

    UI login screen

--- app/src/main/java/com/example/kotlincleanarchitecture/features/login/presentation/screen/LoginScreen.kt
+++ app/src/main/java/com/example/kotlincleanarchitecture/features/login/presentation/screen/LoginScreen.kt
@@ -1,47 +1,90 @@
 package com.example.kotlincleanarchitecture.features.login.presentation.screen
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlincleanarchitecture.features.login.presentation.screen.ui.theme.KotlinCleanArchitectureTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class LoginScreen : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinCleanArchitectureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+@Preview
+@Composable
+fun LoginScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = 14.dp)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            OutlinedTextField(
+                value = "",
+                onValueChange = { it },
+                label =  {
+                    Text(
+                        "Tài khoản",
+                        style = TextStyle(fontSize = 12.sp),
                     )
-                }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(10.dp),
+            )
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { it },
+                label = {
+                    Text(
+                        "Nhập mật khẩu",
+                        style = TextStyle(fontSize = 12.sp),
+                        modifier = Modifier.padding(0.dp)
+                    )
+                },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.fillMaxWidth().padding(top= 15.dp),
+                textStyle = TextStyle(
+                    fontSize = 12.sp
+                )
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth().padding(top= 30.dp),
+                shape= RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0XFF3A59D1)
+                )
+            )
+            {
+                Text(
+                    text = "Đăng nhập",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = Color.White,
+                    ),
+                )
+
             }
+
+
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinCleanArchitectureTheme {
-        Greeting("Android")
-    }
 }
